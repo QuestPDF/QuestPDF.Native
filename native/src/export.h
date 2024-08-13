44d4923b3937f622dca6cf17b33f2cb8aaa6b238
Marcin Ziąbek <marcin@ziabek.com>
2024-08-13 19:53:27 +0200

    Update export.h

--- native/src/export.h
+++ native/src/export.h
@@ -1,5 +1,8 @@
-#ifdef _WIN32
+#if defined(_WIN32)
 #define QUEST_API __declspec(dllexport)
+#elif defined(__EMSCRIPTEN__)
+#include <emscripten.h>
+#define QUEST_API EMSCRIPTEN_KEEPALIVE
 #else
 #define QUEST_API
 #endif
